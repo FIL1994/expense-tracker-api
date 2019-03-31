1b07d2a9596bcbbc5f4df34e8d6e83a439a8d82b
Philip Van Raalte <phil.vanraalte@gmail.com>
2019-03-30 20:33:10 -0400

    added auth guard to get/photo

--- src/photo/photo.module.ts
+++ src/photo/photo.module.ts
@@ -3,9 +3,10 @@ import { PhotoService } from './photo.service';
 import { PhotoController } from './photo.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from './photo.entity';
+import { JWT_MODULE_IMPORTS } from 'src/config/jwt-module-imports';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Photo])],
+  imports: [TypeOrmModule.forFeature([Photo]), ...JWT_MODULE_IMPORTS],
   providers: [PhotoService],
   controllers: [PhotoController],
 })
